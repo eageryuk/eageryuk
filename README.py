2880b3f8779be2f23ee835bb3d8a5b9c3377cb3c
eageryuk <95845299+eageryuk@users.noreply.github.com>
2021-12-09 18:15:40 +1100

    Create README.py

---
+++ README.py
@@ -0,0 +1,8 @@
+- 👋 Hi, I’m @eageryuk
+- 👀 I’m interested in coding
+- 🌱 I’m currently learning python
+- 📫 How to reach me?? 
+Question answered :)
+
+for task in python:
+return "anuragpakhrintheasp@gmail.com is my email"
